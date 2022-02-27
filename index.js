c73014da54e57dbd8035a5afe243aec3ffcfc07e
steelekreis <steele.kreis@gmail.com>
2022-02-26 21:00:54 -0700

    added packages and write function to index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+fs.writeFile('README.md',dataPlaceHolder), err => {
+    if(err) throw err;
+    console.log('Your README has now been generated and can be found at README.md.');
+};
 
 // TODO: Create a function to initialize app
 function init() {}
